58bd10df274285b9e21892083e73ffe14afa1c2a
Giuliano Neves Damasceno <giuliano131@hotmail.com>
2021-05-21 15:52:30 -0300

    resolvendo problema package.json

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const customExpress = require('./config/customExpress.js')
 const PORT = process.env.PORT || 5000
 const app = customExpress()
 
-app.listen(PORT, () => console.log('Servidor rodando na porta 3000'))
+app.listen(PORT, () => console.log('Servidor rodando na porta 3heroku logs --tail000'))
 
 
